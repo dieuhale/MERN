b0ac67a6d60973f89cf51e081720722f2df2892d
“Dieu <dieuhale@gmail.com>
2022-08-05 11:35:53 -0700

    first commit

---
+++ React/firstjsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="First-jsx">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt.Everest</li>
+        <li>Run a Maraton</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
